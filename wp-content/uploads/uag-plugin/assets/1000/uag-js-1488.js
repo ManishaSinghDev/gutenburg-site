1700eefa9fc9336230e39b818cd8ff830ed6bbd0
rivamani <manisha.hope1@gmail.com>
2026-03-09 11:47:09 -0700

    All files added

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1488.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-461228dc' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-cd33e4ed' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-cc514627' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-8bb4b826' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1492.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-02226482' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d862191c' );
+});
+ });
